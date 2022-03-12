cedb63ada75254ce285653f821325141e71a12da
mackogit <mackogit@protonmail.com>
2022-03-12 17:29:44 +0100

    doctum compliant param

--- app/Http/Middleware/JsonResponseMiddleware.php
+++ app/Http/Middleware/JsonResponseMiddleware.php
@@ -11,7 +11,7 @@ class JsonResponseMiddleware
      * Handle an incoming request.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @param  \Closure $next
      * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
      */
     public function handle(Request $request, Closure $next)
