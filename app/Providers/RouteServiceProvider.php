6f4c04883bc33652127c879c88a524671251b7e1
mackogit <mackogit@protonmail.com>
2022-04-04 22:25:13 +0200

    route user can be called for current authenticate user with current keyword

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -26,7 +26,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string|null
      */
-    // protected $namespace = 'App\\Http\\Controllers';
+    protected $namespace = 'App\\Http\\Controllers';
 
     /**
      * Define your route model bindings, pattern filters, etc.
@@ -47,6 +47,14 @@ class RouteServiceProvider extends ServiceProvider
                 ->namespace($this->namespace)
                 ->group(base_path('routes/web.php'));
         });
+
+
+        // Bindings
+        Route::bind('user_id', function($value) {
+            return $value === "current" ? auth()->user : \App\Models\User::whereId($value)->firstOrFail();
+        });
+
+
     }
 
     /**
