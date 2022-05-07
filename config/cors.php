c08c4d1ac88d4442c659d4e1d6dd39e25431d436
mackogit <mackogit@protonmail.com>
2022-05-07 16:37:11 +0200

    change cors policies to accept local applications

--- config/cors.php
+++ config/cors.php
@@ -15,11 +15,16 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie', 'oauth/*'],
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => [
+        'http://localhost:4200',
+        'http://localhost',
+        'http://localhost:106',
+        'http://localhost:190'
+    ],
 
     'allowed_origins_patterns' => [],
 
@@ -29,6 +34,6 @@ return [
 
     'max_age' => 0,
 
-    'supports_credentials' => false,
+    'supports_credentials' => true,
 
 ];
