a93b62bfbfa31560fe54420fd8f9e4e20b26932d
mackogit <mackogit@protonmail.com>
2022-06-11 16:30:31 +0200

    add relation createdBy and pictures to be loaded by request

--- app/Http/Resources/RecipeResource.php
+++ app/Http/Resources/RecipeResource.php
@@ -19,11 +19,15 @@ class RecipeResource extends JsonResource
             'star' => $this->star,
             'base_price' => $this->base_price,
             'description' => $this->description,
+            'created_by' => $this->created_by,
             'recipe_type' => new RecipeTypeResource($this->type),
             'ingredients' => new IngredientCollection($this->ingredients),
             'available_at' => $this->available_at,
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
+            'trashed_at' => $this->trashed_at,
+            'pictures' => FilePublicResource::collection($this->whenLoaded('pictures')),
+            'createdBy' => new UserResource($this->whenLoaded('createdBy')),
             'abilities' => $this->appendAbilities($request)
         ];
     }
--- app/ModelFilters/RecipeFilter.php
+++ app/ModelFilters/RecipeFilter.php
@@ -3,9 +3,21 @@
 namespace App\ModelFilters;
 
 use EloquentFilter\ModelFilter;
+use App\Traits\Filterable\IncludesConstraint;
+use App\Contracts\Filterable\IncludesConstraint as IncludesConstraintContract;
 
-class RecipeFilter extends ModelFilter
+class RecipeFilter extends ModelFilter implements IncludesConstraintContract
 {
+
+    use IncludesConstraint;
+    
+    /**
+     * Relation autorisées à être ajoutées par query parameters
+     *
+     * @var array<string>
+     */
+    public $allowed_includes = ['createdBy', 'pictures'];
+
     /**
     * Related Models that have ModelFilters as well as the method on the ModelFilter
     * As [relationMethod => [input_key1, input_key2]].
